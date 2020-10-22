72a1029867da2b406cd4ca925cb79523bc9a4414
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-22 18:56:24 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,8 +1,7 @@
 def addition(num1, num2)
 
 # addition - Build the method addition that adds num2 to num1 and returns the result of this calculation
- sum_add = num2 + num1
- puts sum_add
+ num2 + num1
 end
 
 def subtraction(num1, num2)
